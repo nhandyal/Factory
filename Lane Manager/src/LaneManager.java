181245715ae65034fdc66dcfec46360389ea2b22
David Cox <mortalofpower@gmail.com>
2012-10-31 19:32:30 -0700

    New Lane Manager Directory

---
+++ Lane Manager/src/LaneManager.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.Timer;
+
+import java.util.*;
+
+public class LaneManager extends JFrame implements ActionListener
+{
+	
+}
