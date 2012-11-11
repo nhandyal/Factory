0f8d2cf4ccb4f44748a3b0d9dad181a562320641
Siddarth Ramesh <smramesh@usc.edu>
2012-11-11 03:00:34 -0800

    PartsManagerUpdate

--- Part Manager/src/PartsManager.java
+++ Part Manager/src/PartsManager.java
@@ -5,10 +5,11 @@ import java.awt.*;
 import java.awt.event.*; 
 
 public class PartsManager extends JFrame implements ActionListener {
-	JPanel partsGrid, secondColumn, thirdColumn; 
-	JLabel partsIconTitle, partsIcon,  partsNameTitle, partsName, partSelectTitle; 
-	JButton firstPartButton; 
-	FirstColumn fC; SecondColumn sC; ThirdColumn tC; 
+ 
+	JPanel partsGrid; 
+	FirstColumn fC; 
+	SecondColumn sC; 
+	ThirdColumn tC; 
 
 	public PartsManager(){
 		partsGrid = new JPanel(new GridLayout(1, 3)); 		
@@ -18,13 +19,34 @@ public class PartsManager extends JFrame implements ActionListener {
 		//Second Column
 		sC = new SecondColumn(); 
 		partsGrid.add(sC); 		
-		//Third Column		
-		add(partsGrid); 
+		//Third Column			
 		tC = new ThirdColumn(); 
-		partsGrid.add(tC); 
+		partsGrid.add(tC); 	
 		
 		
+		tC.getFirstPartButton().addActionListener(this); 
+		tC.getFirstPartButton().setActionCommand("1"); 
+		tC.getSecondPartButton().addActionListener(this); 
+		tC.getSecondPartButton().setActionCommand("2"); 
+		tC.getThirdPartButton().addActionListener(this); 
+		tC.getThirdPartButton().setActionCommand("3"); 
+		tC.getFourthPartButton().addActionListener(this); 
+		tC.getFourthPartButton().setActionCommand("4"); 
+		tC.getFifthPartButton().addActionListener(this); 
+		tC.getFifthPartButton().setActionCommand("5"); 
+		tC.getSixthPartButton().addActionListener(this); 
+		tC.getSixthPartButton().setActionCommand("6"); 
+		tC.getSeventhPartButton().addActionListener(this); 
+		tC.getSeventhPartButton().setActionCommand("7"); 
+		tC.getEighthPartButton().addActionListener(this); 
+		tC.getEighthPartButton().setActionCommand("8"); 
+		tC.getNinthPartButton().addActionListener(this); 
+		tC.getNinthPartButton().setActionCommand("9"); 
+		tC.getTenthPartButton().addActionListener(this); 
+		tC.getTenthPartButton().setActionCommand("10"); 
 		
+		//main panel
+		add(partsGrid); 
 	}
 	
 	
@@ -32,8 +54,7 @@ public class PartsManager extends JFrame implements ActionListener {
 		PartsManager thisPartsManager = new PartsManager(); 
 		thisPartsManager.setTitle("Parts Manager");
 	    thisPartsManager.setSize(450, 350);	
-	    thisPartsManager.setLocationRelativeTo(null);
-	    
+	    thisPartsManager.setLocationRelativeTo(null);	    
 	    thisPartsManager.setVisible(true); 
 	    thisPartsManager.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 
@@ -41,7 +62,7 @@ public class PartsManager extends JFrame implements ActionListener {
 	
 	
 	public void actionPerformed(ActionEvent ae){
-		
+
 		
 	}
 	
