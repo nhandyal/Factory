4f1c317b826a0a8999df3add59dd51f62031a7b8
Chengye Yin <chengyey@usc.edu>
2012-11-14 18:43:41 -0800

    images are in the images folder

---
+++ KitAssemblyManager/KitAssemblyManager/Conveyor.java
@@ -0,0 +1,104 @@
+package KitAssemblyManager;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Conveyor extends FactoryObject
+{
+	
+	Kit inKit;
+	Kit outKit;
+	int inCount = 0;
+	int outCount = 0;
+	int inStartx = 700;
+	int inStarty = 50;
+	int inFinishx = 400;
+	int inFinishy = 50;
+	int outStartx = 300;
+	int outStarty = 50;
+	int outFinishx = -100;
+	int outFinishy = 50;
+	UpdateServer us;
+	public Conveyor(int xpos, int ypos, String image, UpdateServer us)
+	{
+		super(xpos, ypos, image);
+		setImage(image);
+		this.us = us;
+	}
+	
+	public void bringKit()
+	{
+		inKit = new Kit(inStartx,inStarty,"images/kit.png");
+		us.getKits().add(inKit);
+	}
+	
+	public void moveKit()
+	{
+		for (int i = 0; i < us.getCurrentObjects().size(); i++)
+		{	
+			if (us.getCurrentObjects().get(i) == inKit)
+				us.getCurrentObjects().get(i).setPosition(inKit.getPositionX() + (inFinishx - inStartx)/25, inKit.getPositionY() + (inFinishy - inStarty)/25);
+		}
+		
+	}
+	
+	public void takeKit()
+	{
+		for (int i = 0; i < us.getCurrentObjects().size(); i++)
+		{	
+			if (us.getCurrentObjects().get(i) == outKit)
+				us.getCurrentObjects().get(i).setPosition(outKit.getPositionX() + (outFinishx - outStartx)/25, outKit.getPositionY() + (outFinishy - outStarty)/25);
+		}
+	}
+	
+	public Kit getInKit()
+	{
+		return inKit;
+	}
+	
+	public void setInKit(Kit k)
+	{
+		inKit = k;
+	}
+	
+	public Kit getOutKit()
+	{
+		return outKit;
+	}
+	
+	public void setOutKit(Kit k)
+	{
+		outKit = k;
+	}
+	
+	public int getInFinishX()
+	{
+		return inFinishx;
+	}
+	
+	public int getInFinishY()
+	{
+		return inFinishy;
+	}
+	
+	public int getOutStartX()
+	{
+		return outStartx;
+	}
+	
+	public int getOutStartY()
+	{
+		return outStarty;
+	}
+	
+	public boolean kitArrived()
+	{
+		if (inKit == null)
+			return false;
+		if (inKit.getPositionX() == inFinishx && inKit.getPositionY() == inFinishy)
+			return true;
+		else
+			return false;
+	}
+	
+}
---
+++ KitAssemblyManager/KitAssemblyManager/InspectionCamera.java
@@ -0,0 +1,80 @@
+package KitAssemblyManager;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class InspectionCamera extends FactoryObject
+{	
+	int count = 0;
+	int defaultx;
+	int defaulty;
+	int initialx;
+	int initialy;
+	int finalx;
+	int finaly;
+	Kit k;
+	boolean takePicture;
+	boolean isMoving = false;
+	UpdateServer us;
+	int base;
+	public InspectionCamera(int xpos, int ypos, String image, UpdateServer us){
+		super(xpos, ypos, image);
+		defaultx = x;
+		defaulty = y;
+		setImage(image);
+		this.us = us;
+	}
+	
+	public void takePicture(KitStand s, int base){
+		isMoving = true;
+		this.base = base;
+		k = s.getKit();
+		initialx = x;
+		initialy = y;
+		finalx = s.getPositionX();
+		finaly = s.getPositionY();
+	}
+	
+	public void reset(){
+		initialx = x;
+		initialy = y;
+		finalx = defaultx;
+		finaly = defaulty;
+	}
+	
+	public boolean isMoving(){
+		return isMoving;
+	}
+	
+	public void move()
+	{
+		count = us.getCount() - base; 
+		if (count < 25){
+			x += (finalx - initialx)/25;
+			y += (finaly - initialy)/25;
+			count++;
+		}
+		if (count >= 25 && count < 30)
+		{	
+			takePicture = true;
+			count++;
+		}
+		else if (count == 30)
+		{
+			reset();
+			takePicture = false;
+			k.setPicTaken(true);
+			count++;
+		}
+		else if (count >= 30 && count < 56){
+			x += (finalx - initialx)/25;
+			y += (finaly - initialy)/25;
+			count++;
+		}
+		else if (count == 56)
+		{
+			isMoving = false;
+		}
+	}
+}
+
