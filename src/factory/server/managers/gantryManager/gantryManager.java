f75524073c5d529a7225c1d06dd2ae06d9c06ba3
David Cox <davidcox@usc.edu>
2012-11-14 18:47:07 -0800

    Gantry Manager
    Directory created

---
+++ src/factory/server/managers/gantryManager/gantryManager.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import java.awt.geom.*;
+
+import javax.swing.*;
+import javax.swing.Timer;
+
+import java.util.*;
+
+public class gantryManager extends JFrame implements ActionListener{
+	public gantryManager(){
+		
+	}
+}
