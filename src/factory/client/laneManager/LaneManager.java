5fcaeff70b71fea460af65152e9abb8ef4bcb9fe
Timi Okuboyejo <okuboyej@usc.edu>
2012-11-17 06:20:02 -0800

    LM Client Update

--- src/factory/client/laneManager/LaneManager.java
+++ src/factory/client/laneManager/LaneManager.java
@@ -13,8 +13,8 @@ import java.util.*;
 import factory.global.network.*;
 import factory.global.data.*;
 
-public class LaneManager extends JFrame implements ActionListener, NetworkManager
-{
+public class LaneManager extends JFrame implements ActionListener, NetworkManager{
+
 	NetworkBridge nb1;
 	TreeMap<Integer,FactoryObject> frameAnimationData;
 
@@ -58,55 +58,55 @@ public class LaneManager extends JFrame implements ActionListener, NetworkManage
     }
 
     public void registerClientListener(NetworkBridge newBridge, int cID){}
-		public void syncFrame(int cID){}
-		public void updatePartData(TreeMap<Integer, Parts> partData){}
+	public void syncFrame(){}
+	public void updatePartData(TreeMap<Integer, Parts> partData){}
 		
-		// client specific
-		public void mergeChanges(ArrayList<TreeMap<Integer, Boolean>> mapArray, ArrayList<TreeMap<Integer, FactoryObject>> dataArray){
+	// client specific
+	public void mergeChanges(ArrayList<TreeMap<Integer, Boolean>> mapArray, ArrayList<TreeMap<Integer, FactoryObject>> dataArray){
 				
-				if(mapArray.size() == 1){
-						TreeMap<Integer, Boolean> changeMap = mapArray.get(0);
-						TreeMap<Integer, FactoryObject> changeData = dataArray.get(0);
+		if(mapArray.size() == 1){
+			TreeMap<Integer, Boolean> changeMap = mapArray.get(0);
+			TreeMap<Integer, FactoryObject> changeData = dataArray.get(0);
+					
+			// --- change in this block --- //
 						
-						// --- change in this block --- //
-						
-						// iterate over all the keys present in changeMap
-						// after this loop is complete, the frameAnimationData map will be accurately synced with the server copy
-						for(Integer key : changeMap.keySet()){
-								// check the write direction of the change map key
-								if(changeMap.get(key)){
-										// write new factory object to animation frame
-										FactoryObject newAnimationData = changeData.get(key);
-										frameAnimationData.put(key,newAnimationData);
-								}
-								else{
-										// delete the factory object from frameAnimationData
-										frameAnimationData.remove(key);
-								}
-						}
-						
-						// --- End block -- //
+			// iterate over all the keys present in changeMap
+			// after this loop is complete, the frameAnimationData map will be accurately synced with the server copy
+			for(Integer key : changeMap.keySet()){
+				// check the write direction of the change map key
+				if(changeMap.get(key)){
+					// write new factory object to animation frame
+					FactoryObject newAnimationData = changeData.get(key);
+					frameAnimationData.put(key,newAnimationData);
 				}
 				else{
-						System.out.println("Warning: Corrupt frame data");
+					// delete the factory object from frameAnimationData
+					frameAnimationData.remove(key);
 				}
+			}
+					
+			// --- End block -- //
 		}
+		else{
+			System.out.println("Warning: Corrupt frame data");
+		}
+	}
 		
-		public void syncChanges(ArrayList<TreeMap<Integer,FactoryObject>> dataArray){
-				if(dataArray.size() == 1){
-						TreeMap<Integer, FactoryObject> changeData = dataArray.get(0);
-						frameAnimationData = changeData;
-						System.out.println("Sync complete");
-				}
-				else{
-						System.out.println("Warning: Corrupt frame data");
-				}
+	public void syncChanges(ArrayList<TreeMap<Integer,FactoryObject>> dataArray){
+		if(dataArray.size() == 1){
+			TreeMap<Integer, FactoryObject> changeData = dataArray.get(0);
+			frameAnimationData = changeData;
+			System.out.println("Sync complete");
 		}
+		else{
+			System.out.println("Warning: Corrupt frame data");
+		}
+	}
 		
-		// global
-		public void closeNetworkBridge(int bridgeID){
-				nb1.close();
-		}
+	// global
+	public void closeNetworkBridge(int bridgeID){
+		nb1.close();
+	}
 
     public void paint(Graphics g){
     	Graphics2D g2 = (Graphics2D)g;
