f8127b654fdde5e7ecf62479464f53d5cde8d9d3
Siddarth Ramesh <smramesh@usc.edu>
2012-11-16 19:52:37 -0800

    Update

--- src/factory/client/kitManager/ListOfPartsPanel.java
+++ src/factory/client/kitManager/ListOfPartsPanel.java
@@ -1,37 +1,24 @@
 package factory.client.kitManager;
+import factory.global.data.*; 
 import javax.swing.*; 
 import java.awt.*; 
 import java.util.*; 
 
 //don't add to list - prebuilt list and editable
 public class ListOfPartsPanel extends JPanel {
-	JPanel listOfParts; JLabel theList; ArrayList<String> namesOfParts; 
+	JPanel listPanel; 
+	TreeMap<Integer, Parts> updatableListOfParts; 
+	JLabel displayPartsList; 
 	public ListOfPartsPanel(){
-		namesOfParts = new ArrayList<String>(); 
-		listOfParts = new JPanel(); 
-		for(int i = 0; i<namesOfParts.size(); i++){
-			theList = new JLabel(namesOfParts.get(i)); 
-			listOfParts.add(theList); 
-		}
-		add(listOfParts); 
-		
+		updatableListOfParts = new TreeMap<Integer, Parts>(); 
+		displayPartsList = new JLabel("<HTML>Part 1 <BR>Part 2</HTML>"); 
+		listPanel = new JPanel(); 
+		listPanel.add(displayPartsList); 
+		add(listPanel); 
 	}
-	public JPanel getListOfParts() {
-		return listOfParts;
-	}
-	public void setListOfParts(JPanel listOfParts) {
-		this.listOfParts = listOfParts;
-	}
-	public JLabel getTheList() {
-		return theList;
-	}
-	public void setTheList(JLabel theList) {
-		this.theList = theList;
-	}
-	public ArrayList<String> getNamesOfParts() {
-		return namesOfParts;
-	}
-	public void setNamesOfParts(ArrayList<String> namesOfParts) {
-		this.namesOfParts = namesOfParts;
+
+	public TreeMap<Integer, Parts> getUpdatableListOfParts(){
+		return updatableListOfParts; 
 	}
 }
+
