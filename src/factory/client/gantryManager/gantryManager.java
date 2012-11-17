22d4e24d4714c1fb05517c196b26c09e48b6f265
David Cox <mortalofpower@gmail.com>
2012-11-17 11:41:55 -0800

    Gantry Manager Update
    Fitting what's done of the Gantry Manager into the Server Client
    Architecture

--- src/factory/client/gantryManager/gantryManager.java
+++ src/factory/client/gantryManager/gantryManager.java
@@ -1,4 +1,4 @@
-package factory.server.managers.gantryManager;
+package factory.client.laneManager;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -10,139 +10,113 @@ import javax.swing.Timer;
 import java.util.*;
 
 // user packages
+import factory.global.network.*;
 import factory.global.data.*;
-import factory.server.managers.GuiManager;
 
-public class gantryManager extends JFrame implements GuiManager
+public class LaneManager extends JFrame implements ActionListener, NetworkManager
 {
+	NetworkBridge nb1;
+	TreeMap<Integer,FactoryObject> frameAnimationData;
+
 	ImageIcon background;
-	ArrayList<Bin> bins;
-	FOComparator foc;
 	ImageArray images;
 	
 	TreeMap<Integer,Boolean> changeMap;
-	TreeMap<Integer,FactoryObject> temp;
 	TreeMap<Integer,FactoryObject> changeData;
+	TreeMap<Integer,FactoryObject> temp;
 	TreeMap<Integer,FactoryObject> animData;
 
-	public gantryManager(){
-	
-		index = 1;
-
-
-		// Create 10 Bins
-		bins = new ArrayList<Bin>();
-		bins.add(new Bin(348,98,10,index,0));
-		index+=2;
-		bins.add(new Bin(348,98,10,index,1));
-		index+=2;
-		bins.add(new Bin(348,221,10,index,2));
-		index+=2;
-		bins.add(new Bin(348,221,10,index,3));
-		index+=2;
-		bins.add(new Bin(348,345,10,index,4));
-		index+=2;
-		bins.add(new Bin(348,345,10,index,5));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,6));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,7));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,8));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,9));
-		index+=2;
+	public LaneManager(){
 
 		// Create Backgroud Image
-		background = new ImageIcon("LMBG.png");
-		
-		// Create ImageList
-		images = new ImageArray();
+		background = new ImageIcon("bin/factory/global/assets/LMBG.png");
 
-		// Start Counter
-		counter = 0;
-		
-		// Initialize comparator
-		foc = new FOComparator();
+		images = new ImageArray();
 		
 		changeMap = new TreeMap<Integer,Boolean>();
 		changeData = new TreeMap<Integer,FactoryObject>();
 		animData = new TreeMap<Integer,FactoryObject>();
 		temp = new TreeMap<Integer,FactoryObject>();
+		frameAnimationData = new TreeMap<Integer,FactoryObject>();
+		nb1 = new NetworkBridge(this,"aludra.usc.edu",8465,3);
+		nb1.sync();
 	}
 
-	
-/*	public void actionPerformed( ActionEvent ae ) {
-		//This will be called by the Timer	
-		for(int i=0;i<8;i++){
-			if(lanes.get(i).getActive() == true){		// if lane is on
-				if(counter==24){						// every 25th instance of timer
-					int partindx = feeders.get(i/2).getBin().getPart();
-//					System.out.println(partindx);
-					lanes.get(i).addPart(partindx,index);		// create a new part
-					index++;							// add 1 to index
-					feeders.get(i/2).pushPart();		// subtract 1 from feeder counter
-//					System.out.println(feeders.get());
-					counter = 0;						// reset counter
-				}
-				counter++;
-				if(feeders.get(i/2).getPush() == 0){	// if there are 36 parts on the lane
-					laneSwitch(i,i+1);					// turn off lane, turn on next lane
-					counter = 0;						// reset counter
-				}
-			}
-		}
-		update(changeMap,changeData);
+	public static void main(String[] args){
+		LaneManager l = new LaneManager();
+		l.setVisible(true);
+		l.setSize(400,670);
+		l.createBufferStrategy(2);
+		l.setTitle("Lane Manager");
+		l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		new Timer(50,l).start();
+	} //end main
+
+	/*public void actionPerformed( ActionEvent ae ) {
 		repaint();
-    }		*/
+    }
 
-	/*public void sync(TreeMap<Integer,FactoryObject> map){
+    public void registerClientListener(NetworkBridge newBridge, int cID){}
+		public void syncFrame(int cID){}
+		public void updatePartData(TreeMap<Integer, Parts> partData){}
 		
-		// Add Dividers
-		for(int i=0;i<4;i++){
-			map.put(dividers.get(i).getIndex(),dividers.get(i));
+		// client specific
+		public void mergeChanges(ArrayList<TreeMap<Integer, Boolean>> mapArray, ArrayList<TreeMap<Integer, FactoryObject>> dataArray){
+				
+				if(mapArray.size() == 1){
+						TreeMap<Integer, Boolean> changeMap = mapArray.get(0);
+						TreeMap<Integer, FactoryObject> changeData = dataArray.get(0);
+						
+						// --- change in this block --- //
+						
+						// iterate over all the keys present in changeMap
+						// after this loop is complete, the frameAnimationData map will be accurately synced with the server copy
+						for(Integer key : changeMap.keySet()){
+								// check the write direction of the change map key
+								if(changeMap.get(key)){
+										// write new factory object to animation frame
+										FactoryObject newAnimationData = changeData.get(key);
+										frameAnimationData.put(key,newAnimationData);
+								}
+								else{
+										// delete the factory object from frameAnimationData
+										frameAnimationData.remove(key);
+								}
+						}
+						
+						// --- End block -- //
+				}
+				else{
+						System.out.println("Warning: Corrupt frame data");
+				}
+		}
+		
+		public void syncChanges(ArrayList<TreeMap<Integer,FactoryObject>> dataArray){
+				if(dataArray.size() == 1){
+						TreeMap<Integer, FactoryObject> changeData = dataArray.get(0);
+						frameAnimationData = changeData;
+						System.out.println("Sync complete");
+				}
+				else{
+						System.out.println("Warning: Corrupt frame data");
+				}
+		}
+		
+		// global
+		public void closeNetworkBridge(int bridgeID){
+				nb1.close();
 		}
 
-		// Add Bins (if visible)
-		for(int i=0;i<4;i++){
-			if(feeders.get(i).hasBin() == true){
-				map.put(feeders.get(i).getBin().getIndex(),feeders.get(i).getBin());
-				map.put(feeders.get(i).getBin().getPartIcon().getIndex(),feeders.get(i).getBin().getPartIcon());
-				System.out.println("Feeder"+i+"s bin painted");
-//		for(int i=0;i<8;i++){
-//			if(bins.get(i).getVis() == true){
-//				map.put(bins.get(i).getIndex(),bins.get(i));
-//				map.put(bins.get(i).getPartIcon().getIndex(),bins.get(i).getPartIcon());
-			}
-		}
-
-		// Add Lane Content
-		for(int i=0;i<8;i++){
-			// LaneLines
-			for(int j=0;j<3;j++){
-				if(lanes.get(i).getLaneLine(j).getPositionX() <= 334)
-					map.put(lanes.get(i).getLaneLine(j).getIndex(),lanes.get(i).getLaneLine(j));
-			}
-			// Parts (Lanes and Nests)
-			for(int j=0;j<lanes.get(i).getLaneSize();j++)
-				map.put(lanes.get(i).getLanePart(j).getIndex(),lanes.get(i).getLanePart(j));
-			for(int j=0;j<lanes.get(i).getNestSize();j++)
-				map.put(lanes.get(i).getNestPart(j).getIndex(),lanes.get(i).getNestPart(j));
-		}
-	}*/
-
-	/*public void update(TreeMap<Integer,Boolean> changeMap, TreeMap<Integer,FactoryObject> changeData){
-		
-		
-	}*/
-
-/*    public void paint(Graphics g){
+   /* public void paint(Graphics g){
     	Graphics2D g2 = (Graphics2D)g;
+
+//    	System.out.println("Paint Called");
     	
-		sync(animData);
+//		buildMap(animData);
     	
     	Iterator k = changeMap.keySet().iterator();
-		while(k.hasNext()){
+//comment block//	while(k.hasNext()){
 			int i = (Integer) k.next();
 			if(animData.containsKey(i) == false){
 				animData.put(i,changeData.get(i));
@@ -150,22 +124,22 @@ public class gantryManager extends JFrame implements GuiManager
 			else if(changeMap.get(i) == true){
 				animData.put(i,changeData.get(i));
 			}
-		}
+		}	//end block//
 
 		background.paintIcon(this,g2,0,0);
 
 		// Paint Updated List
-		k = animData.keySet().iterator();
+		k = frameAnimationData.keySet().iterator();
 		while(k.hasNext()){
 			int i = (Integer) k.next();
-			if(i != 0 && animData.get(i).getIndex()> 0){
-				if(animData.get(i).getIsLine()== true)	// if object is a line draw a line
-					g2.drawLine(animData.get(i).getPositionX(),animData.get(i).getPositionY(),animData.get(i).getPositionXF(),animData.get(i).getPositionYF());
+			if(i != 0 && frameAnimationData.get(i).getIndex()> 0){
+				if(frameAnimationData.get(i).getIsLine()== true)	// if object is a line draw a line
+					g2.drawLine(frameAnimationData.get(i).getPositionX(),frameAnimationData.get(i).getPositionY(),frameAnimationData.get(i).getPositionXF(),frameAnimationData.get(i).getPositionYF());
 				else{ 										//if object is not a line draw an ImageIcon
-					int img = animData.get(i).getImageIndex();
-					images.getIcon(img).paintIcon(this,g2,animData.get(i).getPositionX(),animData.get(i).getPositionY());
+					int img = frameAnimationData.get(i).getImageIndex();
+					images.getIcon(img).paintIcon(this,g2,frameAnimationData.get(i).getPositionX(),frameAnimationData.get(i).getPositionY());
 				}
 			}
 		}
-    }		*/
+    }*/
 }
--- src/factory/server/managers/gantryManager/gantryRobot.java
+++ src/factory/server/managers/gantryManager/gantryRobot.java
@@ -1,4 +1,4 @@
-package factory.server.managers.gantryRobot;
+package factory.server.managers.gantryManager;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -13,60 +13,16 @@ import java.util.*;
 import factory.global.data.*;
 import factory.server.managers.GuiManager;
 
-public class gantryManager extends FactoryObject implements GuiManager
+public class gantryRobot extends FactoryObject implements GuiManager
 {
-	ImageIcon background;
-	ArrayList<Bin> bins;
-	FOComparator foc;
+	ImageIcon robot;
 	ImageArray images;
 	
-	TreeMap<Integer,Boolean> changeMap;
-	TreeMap<Integer,FactoryObject> temp;
-	TreeMap<Integer,FactoryObject> changeData;
-	TreeMap<Integer,FactoryObject> animData;
+	public gantryRobot(){
 
-	public gantryManager(){
-	
-		index = 1;
-
-
-		// Create 10 Bins
-		bins = new ArrayList<Bin>();
-		bins.add(new Bin(348,98,10,index,0));
-		index+=2;
-		bins.add(new Bin(348,98,10,index,1));
-		index+=2;
-		bins.add(new Bin(348,221,10,index,2));
-		index+=2;
-		bins.add(new Bin(348,221,10,index,3));
-		index+=2;
-		bins.add(new Bin(348,345,10,index,4));
-		index+=2;
-		bins.add(new Bin(348,345,10,index,5));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,6));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,7));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,8));
-		index+=2;
-		bins.add(new Bin(348,470,10,index,9));
-		index+=2;
-
-		// Create Backgroud Image
-		background = new ImageIcon("LMBG.png");
-		
-		// Create ImageList
-		images = new ImageArray();
-
-		// Start Counter
-		counter = 0;
-		
-		// Initialize comparator
-		foc = new FOComparator();
-		
-		changeMap = new TreeMap<Integer,Boolean>();
-		changeData = new TreeMap<Integer,FactoryObject>();
-		animData = new TreeMap<Integer,FactoryObject>();
-		temp = new TreeMap<Integer,FactoryObject>();
 	}
+
+	public void move(int x, int y){
+		
+	}
+}
