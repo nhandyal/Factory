e294b7eaf3ac4ae60393954e9ae0f1b3a14c4395
Nikhil Handyal <nhandyal@gmail.com>
2012-11-18 15:54:03 -0800

    fmg build v0.1

--- src/factory/client/factoryManager/FactoryManagerGUI.java
+++ src/factory/client/factoryManager/FactoryManagerGUI.java
@@ -9,3 +9,37 @@
 
 package factory.client.factoryManager;
 
+// Java classes
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+// import user classes
+
+public class FactoryManagerGUI extends JPanel implements ActionListener{
+		JPanel masterContainer;
+		FactoryManager parent;
+		CardLayout c1;
+		TreeMap<Integer, Parts> factoryParts;
+		ArrayList<Kits> factoryKits;
+		
+		FactoryManagerGUI(FactoryManager parent){
+				// set panel properties
+				this.parent = parent;
+				masterContainer.setLayout
+		}
+		
+		public void mergeParts(TreeMap<Integre, Parts> newParts){
+				factoryParts = newParts;
+		}
+		
+		public void mergeKits(ArrayList<Kits> newKits){
+				factoryKits = newKits;
+		}
+		
+		
+		
+		public void actionPerformed(ActionEvent ae){}
+}
