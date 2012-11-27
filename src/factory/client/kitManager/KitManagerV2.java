9d097c2a1929c35a4f1ac464c0cf20ebe3f9993c
Nikhil Handyal <nhandyal@gmail.com>
2012-11-27 14:51:20 -0800

    temp commit

--- src/factory/client/kitManager/KitManagerV2.java
+++ src/factory/client/kitManager/KitManagerV2.java
@@ -1,100 +1,256 @@
+/*
+** Author: Nikhil Handyal, Siddarth Ramesh
+** Date: 11/26/12
+** Project: Cs200-Factory
+** Description: Kit Manager Code
+** 
+** Pre-Conditions: None
+*/
 package factory.client.kitManager;
-import factory.global.data.*; 
-import factory.global.network.*;
-import java.util.*;
-import javax.swing.*; 
+
+// Java packages
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.BorderFactory; 
+import javax.swing.border.Border;
 import java.awt.*; 
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
 
-public class KitManagerV2 extends JFrame implements ActionListener{
-		JPanel masterPanel, currentKits, createKit, addParts;
-		TreeMap<Integer, Parts> currentList;
-		NetworkBridge nb1;
-		JTextField name, id, description;
-		JButton create;
-		ArrayList<Kits> factoryKits;
-		ImageArray images;
-		ImageIcon kitStruct;
-		ButtonGroup[] buttonGroups;
-		
-		public KitManagerV2(){
-				masterPanel = new JPanel();
-				currentKits = new JPanel();
-				createKit = new JPanel();
-				addParts = new JPanel();
-				images = new ImageArray();
-				kitStruct = images.getIcon(17);
-				create = new JButton("Create Kit");
-				name = new JTextField("Kit Name");
-				id = new JTextField("Kit ID");
-				description = new JTextField("Kit Description");
-				masterPanel.setLayout(new BoxLayout(masterPanel,BoxLayout.X_AXIS));
-				buttonGroups = new ButtonGroup[8];
-				
-				
-		}
-		
-		public static void main(String[] args) {
-		KitManager thisKitManager = new KitManager(); 
-		thisKitManager.setTitle("Kit Manager");
-	    thisKitManager.setSize(750, 350);	
-	    thisKitManager.setLocationRelativeTo(null);
-	    thisKitManager.setVisible(true); 
-	    thisKitManager.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+// user packages
+import factory.global.data.*; 
+import factory.global.network.*;
 
-	}
+
+public class KitManagerV2 extends JFrame implements ActionListener, ListSelectionListener{
+		private static final int PAGE_WIDTH = 650;
+		private static final int PAGE_HEIGHT = 600;
+		private CardLayout c1;
+		private JPanel activeKitsPanel, kitDataPanel, kitStructPanel, partsSelectPanel;
+		private JPanel activeKitsContainer, createKitContainer, masterContainer;
+		private DefaultListModel listModel;
+		private JList kitList;
+		private JButton createNewKit, saveNewKit, deletePart;
+		private JTextField kitName, kitID;
+		private JTextArea kitDesc;
+		private TreeMap<Integer, Parts> parts;
+		private ImageArray images;
+		private Border greyLine;
+		private boolean toggle;
+		private ButtonGroup[] selectedParts;
 		
-		public void buildKits(){
-				JPanel newFrame = new JPanel();
-				newFrame.setLayout(new BoxLayout(newFrame, BoxLayout.Y_AXIS));
-				JLabel temp = new JLabel("Current Kits");
-				newFrame.add(temp);
-				for(Kits kits : factoryKits){
-						JLabel temp2 = new JLabel(kits.getName());
-						newFrame.add(temp2);
-				}
-				currentKits.add(newFrame);
-				currentKits.revalidate();
-				masterPanel.revalidate();
+		KitManagerV2(){
+				// initialize class variables
+				masterContainer = new JPanel();
+				activeKitsContainer = new JPanel();
+				createKitContainer = new JPanel();
+				activeKitsPanel = new JPanel();
+				kitStructPanel = new JPanel();
+				partsSelectPanel = new JPanel();
+				selectedParts = new ButtonGroup[10];
+				parts = buildParts();
+				images = new ImageArray();
+				greyLine = BorderFactory.createLineBorder(Color.DARK_GRAY);
+				c1 = new CardLayout();
+				
+				
+				// set Frame and properties
+				masterContainer.setLayout(c1);
+				
+				
+				// build Active Kits Container
+				activeKitsContainer.setLayout(new BoxLayout(activeKitsContainer, BoxLayout.X_AXIS));
+				setComponentSize(activeKitsContainer,PAGE_WIDTH,PAGE_HEIGHT);
+				buildActiveKits(activeKitsPanel);
+				activeKitsContainer.add(activeKitsPanel);
+				
+				
+				// build Create Kit Container
+				createKitContainer.setLayout(new BoxLayout(createKitContainer, BoxLayout.X_AXIS));
+				setComponentSize(createKitContainer,PAGE_WIDTH,PAGE_HEIGHT);
+				setComponentSize(partsSelectPanel, 450, PAGE_HEIGHT);
+				buildKitStruct(kitStructPanel);
+				buildPartsSelect();
+				createKitContainer.add(kitStructPanel);
+				createKitContainer.add(partsSelectPanel);
+				
+				// add master containers to frame
+				masterContainer.add(activeKitsContainer,"akc");
+				masterContainer.add(createKitContainer,"ckc");
+				this.add(masterContainer);
+				c1.show(masterContainer,"ckc");
+				
 		}
 		
-		public void buildNewKit(){
-				JPanel newFrame = new JPanel();
-				JLabel temp = new JLabel(kitStruct);
-				newFrame.setLayout(new BoxLayout(newFrame, BoxLayout.Y_AXIS));
-				name.setText("Kit Name");
-				id.setText("Kit ID");
-				description.setText("Kit Description");
-				newFrame.add(name);
-				newFrame.add(id);
-				newFrame.add(description);
-				newFrame.add(temp);
-				newFrame.add(create);
-				createKit.add(newFrame);
-				createKit.revalidate();
-				masterPanel.revalidate();
+		public static void main(String[] args){
+				KitManagerV2 km = new KitManagerV2();
+				
+				// set frame properties
+				km.setSize(PAGE_WIDTH,PAGE_HEIGHT);
+				km.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				km.setTitle("Kit Manager");
+				km.setResizable(false);
+				km.setVisible(true);
 		}
 		
-		public void buildPartSelector(){
-				for(int i = 0; i < 8; i++){
-						JPanel container = new JPanel();
-						container.setLayout(new BorderLayout());
-						JLabel temp1 = new JLabel("Part "+i);
-						container.add(temp1, BorderLayout.WEST);
-						buttonGroups[i] = new ButtonGroup();
+		public void valueChanged(ListSelectionEvent le){}
+		public void actionPerformed(ActionEvent ae){
+				if(ae.getSource() == deletePart){
 						
-						for(Integer i : currentList.keySet()){
-								Parts part = currentList.get(i);
-								String name = part.getName();
-								JPanel holder = new JPanel();
-								holder.setLayout(new BoxLayout(holder, BoxLayout.Y_AXIS));
-								JButton 
+				}
+				else{
+						if(toggle){
+								c1.show(masterContainer,"akc");
 						}
+						else{
+								c1.show(masterContainer,"ckc");
+						}
+						toggle = !toggle;
 				}
 		}
 		
-		public void actionPerformed(ActionEvent ae){}
+		// -------------------------------------------------------------------------------------- //
+		// ---------------------------------- Constructor Helpers ------------------------------- //
+		// -------------------------------------------------------------------------------------- //
+		private void buildActiveKits(JPanel container){
+				// initialize variable
+				final int WIDTH = 150;
+				
+				// set containment panel properties
+				container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+				setComponentSize(container,150,PAGE_HEIGHT);
+				
+				
+				JLabel header = new JLabel("Active Kits");
+				header.setHorizontalAlignment(header.CENTER);
+				header.setFont(new Font("Serif", Font.BOLD, 18));
+				setComponentSize(header,WIDTH,25);
+				
+				
+				createNewKit = new JButton("Create New Kit");
+				createNewKit.addActionListener(this);
+				setComponentSize(createNewKit,WIDTH,50);
+				
+				// create list model and list
+				listModel = new DefaultListModel();
+				
+				kitList = new JList(listModel);
+				kitList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        kitList.addListSelectionListener(this);
+				kitList.setFixedCellHeight(25);
+        JScrollPane listScrollPane = new JScrollPane(kitList);
+				
+				// add elements to containment panel
+				container.add(header);
+				container.add(listScrollPane);
+				container.add(createNewKit);
+		}
+		
+		private void buildKitStruct(JPanel container){		
+				// initialize variables
+				final int WIDTH = 200;
+				final int ST_SPACE = 25;
+				kitName = new JTextField();
+				kitID = new JTextField();
+				kitDesc = new JTextArea();
+				saveNewKit = new JButton("Save Kit");
+				deletePart = new JButton("Delete Part");
+				JLabel kitLayout = new JLabel(images.getIcon(17));
+				kitDesc.setBorder(greyLine);
+				saveNewKit.addActionListener(this);
+				deletePart.addActionListener(this);
+				Box box = Box.createVerticalBox();
+				
+				// set container properties
+				setComponentSize(container,WIDTH,600);
+				
+				// set field and button properties
+				setComponentSize(kitName,WIDTH,25);
+				setComponentSize(kitID,WIDTH,25);
+				setComponentSize(kitDesc,WIDTH,100);
+				setComponentSize(saveNewKit,WIDTH,50);
+				setComponentSize(deletePart,WIDTH,50);
+				
+				resetKitStruct();
+				
+				// add elements to container
+				box.add(kitName);
+				box.add(Box.createVerticalStrut(ST_SPACE));
+				box.add(kitID);
+				box.add(Box.createVerticalStrut(ST_SPACE));
+				box.add(kitDesc);
+				box.add(Box.createVerticalStrut(ST_SPACE));
+				box.add(kitLayout);
+				box.add(Box.createVerticalStrut(ST_SPACE));
+				box.add(saveNewKit);
+				box.add(Box.createVerticalStrut(ST_SPACE));
+				box.add(deletePart);
+				container.add(box);
+				//container.setBorder(blueLine);
+		}
+		
+		private void buildPartsSelect(){
+				final int H_WIDTH = 450;
+				final int H_HEIGHT = 60;
+				//final int n = parts.size()+1;
+				int n = 10;
+				Box box = Box.createVerticalBox();
+				partsSelectPanel.removeAll();
+				
+				
+				for(int i = 0; i < 8; i++){
+						JPanel holder = new JPanel();
+						holder.setLayout(new BorderLayout());
+						setComponentSize(holder, H_WIDTH, H_HEIGHT);
+						JLabel partLabel = new JLabel("   Part "+(i+1)+":   ");
+						
+						// build parts selector
+						JPanel PSelectorContainer = new JPanel();
+						PSelectorContainer.setLayout(new GridLayout(1,n));
+						selectedParts[i] = new ButtonGroup();
+						for(int j = 0; j < n; j++){
+								Box box2 = Box.createVerticalBox();
+								JLabel temp = new JLabel("  ",images.getIcon(j), SwingConstants.CENTER);
+								temp.setHorizontalTextPosition(SwingConstants.LEFT);
+								JRadioButton partButton = new JRadioButton();
+								selectedParts[i].add(partButton);
+								box2.add(temp);
+								box2.add(partButton);
+								PSelectorContainer.add(box2);
+						}
+						holder.add(partLabel, BorderLayout.WEST);
+						holder.add(PSelectorContainer, BorderLayout.CENTER);
+						partsSelectPanel.add(holder);
+				}
+				partsSelectPanel.revalidate();
+		}
+		
+		// -------------------------------------------------------------------------------------- //
+		// ---------------------------------------------- Helpers ------------------------------- //
+		// -------------------------------------------------------------------------------------- //
+		
+		private void setComponentSize(JComponent component, int w, int h){
+				component.setMinimumSize(new Dimension(w,h));
+				component.setMaximumSize(new Dimension(w,h));
+				component.setPreferredSize(new Dimension(w,h));
+				component.setAlignmentX(Component.LEFT_ALIGNMENT);
+		}
+		
+		private void resetKitStruct(){
+				kitName.setText("Enter Kit Name");
+				kitID.setText("Enter Kit ID");
+				kitDesc.setText("Enter a short kit description");
+		}
+		
+		private TreeMap<Integer, Parts> buildParts(){
+				TreeMap<Integer, Parts> partsMap = new TreeMap<Integer, Parts>();
+				for(int i = 0; i < 5; i++){
+						Parts temp = new Parts(i,"Test Part: "+i, "Test Part "+i+" description", i);
+						temp.setMapIndex(i);
+						partsMap.put(i,temp);
+				}
+				return partsMap;
+		}
 }
